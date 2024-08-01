dc0c1ea1ba9bcda694c2beaecb9656e4a8dad77f
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2024-08-01 10:10:21 +0700

    Layouts and nested routes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
 } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
 
 // //Old Way
 // const routeDefinitions = createRoutesFromElements(
@@ -19,8 +20,14 @@ import ProductsPage from "./pages/Products";
 
 //New Way
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "products", element: <ProductsPage /> },
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "products", element: <ProductsPage /> },
+    ],
+  },
 ]);
 
 function App() {
---
+++ src/pages/Root.js
@@ -0,0 +1,18 @@
+//Outlet ini untuk render children router dimana akan di tampilkan
+import { Outlet } from "react-router-dom";
+
+import MainNavigation from "../components/MainNavigation";
+import classes from "./Root.module.css";
+
+function RootLayout() {
+  return (
+    <>
+      <MainNavigation />
+      <main className={classes.content}>
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
+export default RootLayout;
